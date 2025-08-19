2f4150ee3652c0ef31ddb13ea864257da64f4cac
NgaLe02 <ngalee2002@gmail.com>
2025-08-19 23:26:14 +0700

    .

--- csdlnc-front/src/App.js
+++ csdlnc-front/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import System from "./app/page/system/System";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Thanh tab */}
+        <nav className="App-header">
+          <Link className="App-link" to="/system">
+            System
+          </Link>{" "}
+          |{" "}
+          <Link className="App-link" to="/revenue">
+            Revenue
+          </Link>
+        </nav>
+
+        {/* Nội dung hiển thị theo route */}
+        <Routes>
+          <Route path="/system" element={<System />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- csdlnc-front/src/app/page/system/System.tsx
+++ csdlnc-front/src/app/page/system/System.tsx
@@ -1,3 +1,38 @@
+import { HttpStatusCode } from "axios";
+import { CarService } from "../../services/CarService";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 export default function System() {
+  const [listCar, setListCar] = useState([]);
+
+  useEffect(() => {
+    getLstCar();
+  }, []);
+
+  function getLstCar() {
+    CarService.getInstance()
+      .getLstCar({})
+      .then((response) => {
+        if (response.status === HttpStatusCode.Ok) {
+          if (response.data.status) {
+            const data = response.data.responseData;
+            // const mappedData = data.map((item: any) => ({
+            //     value: item.id,
+            //     name: item.name,
+            // }));
+            setListCar(data);
+          } else {
+            toast.error("Tìm kiếm không thành công");
+          }
+        } else {
+          toast.error("Tìm kiếm không thành công");
+        }
+      })
+      .catch((error) => {
+        toast.error("Tìm kiếm không thành công");
+      });
+  }
+
   return <></>;
 }
