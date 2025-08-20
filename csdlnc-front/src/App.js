2dfbe193bc9ba656b56f57fb81d5af2a10ddf58b
NgaLe02 <ngalee2002@gmail.com>
2025-08-20 17:51:50 +0700

    .

--- csdlnc-front/src/App.js
+++ csdlnc-front/src/App.js
@@ -1,25 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./app/page/layout/Header";
 import System from "./app/page/system/System";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* Thanh tab */}
-        <nav className="App-header">
-          <Link className="App-link" to="/system">
-            System
-          </Link>{" "}
-          |{" "}
-          <Link className="App-link" to="/revenue">
-            Revenue
-          </Link>
-        </nav>
+        <Header />
 
-        {/* Nội dung hiển thị theo route */}
         <Routes>
           <Route path="/system" element={<System />} />
+          {/* bạn có thể thêm Route cho Revenue ở đây */}
         </Routes>
       </div>
     </Router>
