81ccfd58751f4ab9c221308d55e0e5faf462a5a7
NgaLe02 <ngalee2002@gmail.com>
2025-08-29 20:39:49 +0700

    .

--- csdlnc-back/src/main/java/com/ptit/csdlnc/model/Assignment.java
+++ csdlnc-back/src/main/java/com/ptit/csdlnc/model/Assignment.java
@@ -1,11 +1,10 @@
 package com.ptit.csdlnc.model;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
