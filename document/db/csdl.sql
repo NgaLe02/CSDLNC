f12a0b135a9ace95cd1ece58f8e0d8e6b6d03848
NgaLe02 <ngalee2002@gmail.com>
2025-09-03 22:29:02 +0700

    .

--- document/db/csdl.sql
+++ document/db/csdl.sql
@@ -52,7 +52,8 @@ CREATE TABLE HanDangKiem (
     maHanDangKiem INT PRIMARY KEY AUTO_INCREMENT,
     chiPhi DECIMAL(12,2) CHECK (chiPhi >= 0),
     ngayDangKiem DATE NOT NULL,
-    maXe varCHAR(4),	
+    hieuLuc INT NOT NULL, --month
+    maXe varCHAR(4),	    
 	CONSTRAINT uq_xe_ngay UNIQUE (maXe, ngayDangKiem),
     FOREIGN KEY (maXe) REFERENCES Xe(maXe)
 );
